06e6b2e83f6e9c4bd92a7a1bdc01b2052d90923b
wqhqq1 <53130008+wqhqq1@users.noreply.github.com>
2021-02-25 11:31:49 +0800

    Update OcBootManagementLib.c

--- Library/OcBootManagementLib/OcBootManagementLib.c
+++ Library/OcBootManagementLib/OcBootManagementLib.c
@@ -472,6 +472,7 @@ OcShowSimplePasswordRequest (
                );
     for(Index=0; Index<SHA512_DIGEST_SIZE; ++Index) {
         CHAR16 a[1] = {VerifyHash[Index]+'0'};
+        gST->ConOut->OutputString (gST->ConOut, "\r\nhash:");
         gST->ConOut->OutputString (gST->ConOut, a);
         gST->ConOut->OutputString (gST->ConOut, L"\r\n");
     }
